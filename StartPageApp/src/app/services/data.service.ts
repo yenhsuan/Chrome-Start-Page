1767a1c0bb5ec346f80d75c80b8303088a2edfc2
yenhsuan <yenhsuan.terry@gmail.com>
2017-08-19 15:12:17 -0700

    update: right-click menu, data service

--- StartPageApp/src/app/services/data.service.ts
+++ StartPageApp/src/app/services/data.service.ts
@@ -4,10 +4,35 @@ import { BehaviorSubject, Observable } from 'rxjs/Rx';
 @Injectable()
 export class DataService {
 
-  iconDataArray = new BehaviorSubject<Array<object>>([]);
-
+  // iconDataArray = new BehaviorSubject<Array<object>>([]);
+  iconArray: Array<object>;
   constructor() {
-    this.iconDataArray.next([
+
+    let iconStr = this.loadIconsFromLocalStorage();
+    if (!iconStr) {
+      iconStr = this.loadDefaultIcons();
+    }
+
+    this.iconArray = JSON.parse(iconStr);
+  }
+
+  updateIconsArrayToLocalStorage(): void {
+    localStorage.setItem('iconArray', JSON.stringify(this.iconArray));
+  }
+
+  loadIconsFromLocalStorage(): string {
+    const str = localStorage.getItem('iconArray');
+    if (str) {
+      return str;
+    }
+
+    return '';
+  }
+
+
+  loadDefaultIcons(): string {
+
+    const defaultIcons = [
       {
         name: 'Facebook',
         url: 'https://www.facebook.com',
@@ -37,44 +62,16 @@ export class DataService {
         url: 'https://www.linkedin.com',
         iconUrl: '../../assets/icon-alt/LinkedIn.png',
         corner: 0
-      },
-      {
-        name: 'Facebook',
-        url: 'https://www.facebook.com',
-        iconUrl: '../../assets/icon-alt/Facebook.png',
-        corner: 0
-      },
-      {
-        name: 'Google',
-        url: 'https://www.facebook.com',
-        iconUrl: '../../assets/icon-alt/Google.png',
-        corner: 0
-      },
-      {
-        name: 'Youtube',
-        url: 'https://www.youtube.com',
-        iconUrl: '../../assets/icon-alt/Youtube I.png',
-        corner: 0
-      },
-      {
-        name: 'Gmail',
-        url: 'https://mail.google.com',
-        iconUrl: '../../assets/icon-alt/Gmail.png',
-        corner: 0
-      },
-      {
-        name: 'LinkedIn',
-        url: 'https://www.linkedin.com',
-        iconUrl: '../../assets/icon-alt/LinkedIn.png',
-        corner: 0
-      },
+      }
+    ];
 
-    ]);
-
-
+    return JSON.stringify(defaultIcons);
   }
 
-  subscribeIconData(): Observable<Array<object>> {
-    return this.iconDataArray.asObservable();
-  }
+
+
+
+  // subscribeIconData(): Observable<Array<object>> {
+  //   return this.iconDataArray.asObservable();
+  // }
 }
