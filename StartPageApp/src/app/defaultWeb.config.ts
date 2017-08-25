f6b3987c4c9b756e6294b576a7bbb1c579b13bbd
Vivian <vivian@ViviandeMacBook-Pro.local>
2017-08-25 15:26:16 -0700

    update default web

--- StartPageApp/src/app/defaultWeb.config.ts
+++ StartPageApp/src/app/defaultWeb.config.ts
@@ -1,5 +1,109 @@
 export const defaultWebsite = [
   {
+    name: '9GAG',
+    url: 'https://9gag.com',
+    iconUrl: '../assets/icon-alt/9Gag.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Amazon',
+    url: 'https://www.amazon.com',
+    iconUrl: '../assets/icon-alt/Amazon.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Android',
+    url: 'https://www.android.com',
+    iconUrl: '../assets/icon-alt/Android.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Apple',
+    url: 'https://www.apple.com',
+    iconUrl: '../assets/icon-alt/Apple.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'App Store',
+    url: 'https://itunes.apple.com',
+    iconUrl: '../assets/icon-alt/Appstore.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'AWS',
+    url: 'https://aws.amazon.com',
+    iconUrl: '../assets/icon-alt/AWS.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Bing',
+    url: 'https://www.bing.com',
+    iconUrl: '../assets/icon-alt/Bing.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Blogger',
+    url: 'https://www.blogger.com',
+    iconUrl: '../assets/icon-alt/Blogger.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'CloudApp',
+    url: 'https://www.getcloudapp.com',
+    iconUrl: '../assets/icon-alt/Cloudapp.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'CSS3',
+    url: 'http://www.css3.info',
+    iconUrl: '../assets/icon-alt/CSS3.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Dropbox',
+    url: 'https://www.dropbox.com',
+    iconUrl: '../assets/icon-alt/Dropbox.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'eBay',
+    url: 'https://www.ebay.com',
+    iconUrl: '../assets/icon-alt/Ebay.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Evernote',
+    url: 'https://evernote.com',
+    iconUrl: '../assets/icon-alt/Evernote.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
     name: 'Facebook',
     url: 'https://www.facebook.com',
     iconUrl: '../assets/icon-alt/Facebook.png',
@@ -8,9 +112,425 @@ export const defaultWebsite = [
     showIconCase: false
   },
   {
+    name: 'Firefox',
+    url: 'https://www.mozilla.org/en-US/firefox',
+    iconUrl: '../assets/icon-alt/Firefox.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Flickr',
+    url: 'https://www.flickr.com',
+    iconUrl: '../assets/icon-alt/Flickr.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com',
+    iconUrl: '../assets/icon-alt/Github.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
     name: 'Gmail',
     url: 'https://www.gmail.com',
     iconUrl: '../assets/icon-alt/Gmail.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Google Chrome',
+    url: 'https://www.google.com/chrome/browser/desktop/index.html',
+    iconUrl: '../assets/icon-alt/Google\ Chrome.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Google Drive',
+    url: 'https://www.google.com/drive',
+    iconUrl: '../assets/icon-alt/Google\ Drive.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Google Play',
+    url: 'https://play.google.com/store',
+    iconUrl: '../assets/icon-alt/Google\ Play.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Google Plus',
+    url: 'https://plus.google.com',
+    iconUrl: '../assets/icon-alt/Google\ Plus.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Google',
+    url: 'https://www.google.com',
+    iconUrl: '../assets/icon-alt/Google.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Hangouts',
+    url: 'https://hangouts.google.com',
+    iconUrl: '../assets/icon-alt/Hangouts.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com',
+    iconUrl: '../assets/icon-alt/Instagram.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'iTunes',
+    url: 'https://www.apple.com/itunes',
+    iconUrl: '../assets/icon-alt/iTunes.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Line',
+    url: 'https://line.me',
+    iconUrl: '../assets/icon-alt/Line.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com',
+    iconUrl: '../assets/icon-alt/LinkedIn.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'MSN',
+    url: 'http://www.msn.com',
+    iconUrl: '../assets/icon-alt/MSN.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'MySpace',
+    url: 'https://myspace.com',
+    iconUrl: '../assets/icon-alt/MySpace.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Opera',
+    url: 'http://www.opera.com',
+    iconUrl: '../assets/icon-alt/Opera.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Outlook',
+    url: 'https://outlook.live.com',
+    iconUrl: '../assets/icon-alt/Outlook.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'PayPal',
+    url: 'https://www.paypal.com',
+    iconUrl: '../assets/icon-alt/PayPal.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Picasa',
+    url: 'https://picasa.google.com',
+    iconUrl: '../assets/icon-alt/Picasa.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Pinboard',
+    url: 'https://pinboard.in',
+    iconUrl: '../assets/icon-alt/Pinboard.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Pinterest',
+    url: 'https://www.pinterest.com',
+    iconUrl: '../assets/icon-alt/Pinterest.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'PlayStation',
+    url: 'https://www.playstation.com',
+    iconUrl: '../assets/icon-alt/Playstation.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Reddit',
+    url: 'https://www.reddit.com',
+    iconUrl: '../assets/icon-alt/Reddit.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'RSS',
+    url: 'https://www.rss.com',
+    iconUrl: '../assets/icon-alt/RSS.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Safari',
+    url: 'https://www.apple.com/safari',
+    iconUrl: '../assets/icon-alt/Safari.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Skype',
+    url: 'https://www.skype.com',
+    iconUrl: '../assets/icon-alt/Skype.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Slack',
+    url: 'https://slack.com',
+    iconUrl: '../assets/icon-alt/Slack.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Snapchat',
+    url: 'https://www.snapchat.com',
+    iconUrl: '../assets/icon-alt/Snapchat.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'SoundCloud',
+    url: 'https://soundcloud.com',
+    iconUrl: '../assets/icon-alt/Soundcloud.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Spotify',
+    url: 'https://www.spotify.com',
+    iconUrl: '../assets/icon-alt/Spotify.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Stack Overflow',
+    url: 'https://stackoverflow.com',
+    iconUrl: '../assets/icon-alt/Stackoverflow.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'StumbleUpon',
+    url: 'https://www.stumbleupon.com',
+    iconUrl: '../assets/icon-alt/Stumbleupon.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Tumblr',
+    url: 'https://www.tumblr.com',
+    iconUrl: '../assets/icon-alt/Tumblr.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com',
+    iconUrl: '../assets/icon-alt/Twitter\ I.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Uber',
+    url: 'https://www.uber.com',
+    iconUrl: '../assets/icon-alt/Uber.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Viber',
+    url: 'https://www.viber.com',
+    iconUrl: '../assets/icon-alt/Viber.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Vimeo',
+    url: 'https://vimeo.com',
+    iconUrl: '../assets/icon-alt/Vimeo.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Vine',
+    url: 'https://vine.co',
+    iconUrl: '../assets/icon-alt/Vine.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'W3C',
+    url: 'https://www.w3.org',
+    iconUrl: '../assets/icon-alt/W3C.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Wallet',
+    url: 'https://wallet.google.com',
+    iconUrl: '../assets/icon-alt/Wallet.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'WeChat',
+    url: 'https://web.wechat.com',
+    iconUrl: '../assets/icon-alt/Wechat.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'WhatsApp',
+    url: 'https://www.whatsapp.com',
+    iconUrl: '../assets/icon-alt/Whatsapp.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Windows Store',
+    url: 'https://www.microsoft.com/store/apps',
+    iconUrl: '../assets/icon-alt/Windows\ Store.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Windows',
+    url: 'https://www.microsoft.com/en-us/windows',
+    iconUrl: '../assets/icon-alt/Windows.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'WordPress',
+    url: 'https://wordpress.com',
+    iconUrl: '../assets/icon-alt/WordPress.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'World Wildlife Fund',
+    url: 'https://www.worldwildlife.org',
+    iconUrl: '../assets/icon-alt/WWF.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'xbox',
+    url: 'https://www.xbox.com',
+    iconUrl: '../assets/icon-alt/Xbox.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Yahoo',
+    url: 'https://www.yahoo.com',
+    iconUrl: '../assets/icon-alt/Yahoo.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Yammer',
+    url: 'https://www.yammer.com',
+    iconUrl: '../assets/icon-alt/Yammer.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Yelp',
+    url: 'https://www.yelp.com',
+    iconUrl: '../assets/icon-alt/Yelp.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Youtube',
+    url: 'https://www.youtube.com',
+    iconUrl: '../assets/icon-alt/Youtube\ II.png',
+    bgColor: '',
+    corner: 0,
+    showIconCase: false
+  },
+  {
+    name: 'Zynga',
+    url: 'https://www.zynga.com',
+    iconUrl: '../assets/icon-alt/Zynga.png',
     bgColor: '',
     corner: 0,
     showIconCase: false
